30aac63a44eb697bc3cf36ca7831f13b2a64bbcb
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-12 14:52:35 +0300

    init application layout

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <div>Header Component</div>;
+
+export default Header;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 class StorePicker extends Component {
 	render() {
 		return (
