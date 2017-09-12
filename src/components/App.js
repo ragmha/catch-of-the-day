8bec3b3537e7107928226e35ed1bc3e1cec3db77
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-12 15:26:43 +0300

    passing dynamic data with props

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
-					<Header />
+					<Header tagline="Fresh SeaFood Market" />
 				</div>
 				<Order />
 				<Inventory />
